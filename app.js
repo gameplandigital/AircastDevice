b21e48f216c336645ff9d913da89ee7b29cb0fb8
gameplandigital <gameplandigital@palmsolutions.co>
2018-05-16 17:07:24 +0800

    asd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var aircast = require('./aircastServer.js')
 
 var Twit = require('twit');
 
-console.log(' HHHH ');
+console.log(' WWW ');
 
 
 // const testFolder = path.join(__dirname, '/Aircast');
