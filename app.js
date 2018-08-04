b86788f9fc45fd9e7bea686d327df858495be485
Jeric <jgui1129.jg@gmail.com>
2018-08-04 11:08:43 +0800

    offlide-mode-fix

--- app.js
+++ app.js
@@ -73,20 +73,20 @@ app.post('/localContent',function (req,res) {
       try{
         if (d.status) {
           fs.writeFile('./../AircastConfig/offline-content.txt', JSON.stringify(d.content), function(err, data){
-              if (err) {
+              //if (err) {
                 res.json({success: false, content: [], err});  
-              }else{
+              //}else{
                 console.log("Successfully Written to File.");
                 res.json({success: true, content: d.content, err});  
-              }
+              //}
           });
         } else {
           fs.readFile('./../AircastConfig/offline-content.txt', 'utf-8' ,function(err, data) {
-            if (err) {
+            //if (err) {
                 res.json({success: false, content: [], err});  
-            }else{
+            //}else{
               res.json({success: true, content: JSON.parse(data), err})
-            }
+            //}
           });
         }
       }catch(error){
@@ -98,13 +98,13 @@ app.post('/localContent',function (req,res) {
 
           let data_to_write = JSON.stringify(d.content);
           fs.writeFile('./../AircastConfig/offline-content.txt',data_to_write, (err) => {
-              if (err) {
+              //if (err) {
                 res.json({success: false, content: [], err});  
-              }else {
+              //}else {
                   console.log("Successfully Written to File.");
                   res.json({success: true, content: JSON.parse(data_to_write), err});
 
-                }
+                //}
           });   
 
         }catch(error){
