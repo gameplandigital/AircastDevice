3a90e5c287ba33cdbf9be8acccad06bf0094a3f9
Tahori <girocalvario@gmail.com>
2019-06-11 16:37:27 +0800

    Change JW Player account, optimize temp0.js and mute programmatic default video

--- public/scripts/temp0.js
+++ public/scripts/temp0.js
@@ -1,6 +1,14 @@
 function temp0Controller($scope, $window, $timeout, $http, temp1Src, callback) {
   var CampaignID = $scope.currentPlayingCampaignID;
 
+  $.ajax({
+    method: "GET",
+    url: "/update_programmatic/video/" + $scope.currentPlayingCampaignID,
+    success: function(res) {
+      console.log(res);
+    }
+  });
+
   jwplayer("mediaPlayer")
     .setConfig({
       repeat: false,
@@ -19,7 +27,7 @@ function temp0Controller($scope, $window, $timeout, $http, temp1Src, callback) {
       }
     })
     .on("adPlay", function() {
-      mediaPlayer.setConfig({
+      this.setConfig({
         mute: false,
         volume: 100
       });
@@ -31,12 +39,4 @@ function temp0Controller($scope, $window, $timeout, $http, temp1Src, callback) {
       callback();
     })
     .play();
-
-  $.ajax({
-    method: "GET",
-    url: "/update_programmatic/video/" + $scope.currentPlayingCampaignID,
-    success: function(res) {
-      console.log(res);
-    }
-  });
 }
